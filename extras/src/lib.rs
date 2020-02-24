915379b88743f8174953aeaff02a6c034267c9d8
Kevin Hoffman <alothien@gmail.com>
2020-02-24 17:37:24 -0500

    Adding sample module for extras capability

---
+++ extras/src/lib.rs
@@ -0,0 +1,39 @@
+// Copyright 2015-2019 Capital One Services, LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+extern crate wascc_actor as actor;
+
+#[macro_use]
+extern crate serde_json;
+
+use actor::prelude::*;
+
+actor_receive!(receive);
+
+pub fn receive(ctx: &CapabilitiesContext, operation: &str, msg: &[u8]) -> ReceiveResult {
+    match operation {
+        http::OP_HANDLE_REQUEST => display_extras(ctx, msg),
+        core::OP_HEALTH_REQUEST => Ok(vec![]),
+        _ => Err("Unknown operation".into()),
+    }
+}
+
+fn display_extras(ctx: &CapabilitiesContext, _payload: impl Into<http::Request>) -> ReceiveResult {
+    let result = json!(
+    { "random": ctx.extras().get_random(0, 100)?,
+      "guid" : ctx.extras().get_guid()?,
+      "sequence": ctx.extras().get_sequence_number()?,
+    });
+    Ok(protobytes(http::Response::json(result, 200, "OK"))?)
+}
