77ae8afa518aa09e8456ca92bd9f77036511217d
Kevin Hoffman <alothien@gmail.com>
2020-04-08 09:07:53 -0400

    Adding JSON output to the AssemblyScript sample

--- kvcounter-as/assembly/index.ts
+++ kvcounter-as/assembly/index.ts
@@ -1,24 +1,35 @@
 import { handleCall, consoleLog, handleAbort } from "wapc-guest-as";
 import { Request, Response, ResponseBuilder, Handlers } from "wascc-actor-as/httpserver";
 import { Host as KV } from "wascc-actor-as/kv";
-
-export function _start(): void {
-  Handlers.handleRequest(handleRequest);
-}
+import { JSONEncoder } from "assemblyscript-json";
 
 function handleRequest(request: Request): Response {
   const kv = new KV("");
   const key = request.path.replaceAll("/", ":");      
-  const result = kv.atomicAdd(key, 1);
-    
-  const message = "Counter is " + result.value.toString();  
-  const payload = String.UTF8.encode(message);
+  const result = kv.atomicAdd(key, 1);  
+     
+  let encoder = new JSONEncoder();
+
+  // Construct output JSON
+  encoder.pushObject("");
+  encoder.setInteger("count", result.value);  
+  encoder.popObject();
+
+  // Get serialized data
+  let json: Uint8Array = encoder.serialize();
 
   return new ResponseBuilder()
     .withStatusCode(200)
     .withStatus("OK")
-    .withBody(payload)
+    .withBody(json.buffer)
     .build();
+}
+
+
+// Ceremony required for module entry points
+
+export function _start(): void {
+  Handlers.handleRequest(handleRequest);
 }
 
 export function __guest_call(operation_size: usize, payload_size: usize): bool {
