2b136287b0124ce4a4a52cc725993d914d9325de
Brooks Townsend <brooksmtownsend@gmail.com>
2021-01-22 16:48:15 -0500

    non-working implementation of kvcounter-as with actor interfaces

--- kvcounter-as/assembly/index.ts
+++ kvcounter-as/assembly/index.ts
@@ -1,18 +1,27 @@
-import { handleCall, consoleLog, handleAbort } from "wapc-guest-as";
-import { Request, Response, ResponseBuilder, Handlers } from "wascc-actor-as/httpserver";
-import { Host as KV } from "wascc-actor-as/kv";
+import { Request, Response, ResponseBuilder, Handlers as HTTPHandlers } from "../../../actor-interfaces/assemblyscript/http-server/assembly/module";
+import { Host as KV } from "../../../actor-interfaces/assemblyscript/keyvalue/assembly/module";
+import { HealthCheckResponse, HealthCheckRequest, Handlers as CoreHandlers } from "../../../actor-interfaces/assemblyscript/actor-core/assembly/module";
 import { JSONEncoder } from "assemblyscript-json";
 
-function handleRequest(request: Request): Response {
+export function wapc_init(): void {
+  CoreHandlers.registerHealthRequest(HealthCheck);
+  HTTPHandlers.registerHandleRequest(HandleRequest);
+}
+
+function HealthCheck(request: HealthCheckRequest): HealthCheckResponse {
+  return new HealthCheckResponse();
+}
+
+function HandleRequest(request: Request): Response {
   const kv = new KV("");
-  const key = request.path.replaceAll("/", ":");      
-  const result = kv.atomicAdd(key, 1);  
-     
+  const key = request.path.replace("/", ":");
+  const result = kv.Add(key, 1);
+
   let encoder = new JSONEncoder();
 
   // Construct output JSON
   encoder.pushObject("");
-  encoder.setInteger("count", result.value);  
+  encoder.setInteger("count", result.value);
   encoder.popObject();
 
   // Get serialized data
@@ -25,23 +34,19 @@ function handleRequest(request: Request): Response {
     .build();
 }
 
-
-// Ceremony required for module entry points
-
-export function _start(): void {
-  Handlers.handleRequest(handleRequest);
-}
+// Boilerplate code for waPC.  Do not remove.
+import { handleCall, handleAbort } from "@wapc/as-guest";
 
 export function __guest_call(operation_size: usize, payload_size: usize): bool {
   return handleCall(operation_size, payload_size);
 }
 
-// Abort function - this should probably be in the actor SDK and not in an actor...
-export function abort(
-    message: string | null,
-    fileName: string | null,
-    lineNumber: u32,
-    columnNumber: u32
-  ): void {
-    handleAbort(message, fileName, lineNumber, columnNumber);
-  }
+// Abort function
+function abort(
+  message: string | null,
+  fileName: string | null,
+  lineNumber: u32,
+  columnNumber: u32
+): void {
+  handleAbort(message, fileName, lineNumber, columnNumber);
+}
