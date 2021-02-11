bf179eb74bd57a91b4cd4320e47d8d10380c2927
Kevin Hoffman <alothien@gmail.com>
2021-02-11 09:32:07 -0500

    Updating subscriber example

--- subscriber/src/lib.rs
+++ subscriber/src/lib.rs
@@ -1,30 +1,29 @@
-// Copyright 2015-2020 Capital One Services, LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// The sample subscriber actor illustrates handling
+// a message from an externally-configured subscription
+// and then prints the contents of that message to
+// stdout using the `wasmcloud:logging` capability.
 
-extern crate wascc_actor as actor;
+extern crate wapc_guest as guest;
+use actorcore::HealthCheckResponse;
+use guest::prelude::*;
+use log::info;
+use messaging::BrokerMessage;
+use wasmcloud_actor_core as actorcore;
+use wasmcloud_actor_logging as logging;
+use wasmcloud_actor_messaging as messaging;
 
-use actor::prelude::*;
-use codec::messaging::BrokerMessage;
-
-actor_handlers!{ codec::messaging::OP_DELIVER_MESSAGE => handle_message, 
-                 codec::core::OP_HEALTH_REQUEST => health }
+#[no_mangle]
+pub fn wapc_init() {
+    actorcore::Handlers::register_health_request(health);
+    messaging::Handlers::register_handle_message(handle_message);
+    logging::enable_macros();
+}
 
 fn handle_message(msg: BrokerMessage) -> HandlerResult<()> {
-    println(&format!("Received message broker message: {:?}", msg));
+    info!("Received message broker message: {:?}", msg);
     Ok(())
 }
 
-fn health(_req: codec::core::HealthRequest) -> HandlerResult<()> {
-    Ok(())
+fn health(_req: actorcore::HealthCheckRequest) -> HandlerResult<HealthCheckResponse> {
+    Ok(HealthCheckResponse::healthy())
 }
