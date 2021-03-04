df9c5b1e20964294cffa63cfc8ca495f78234840
Brooks Townsend <brooksmtownsend@gmail.com>
2021-03-04 17:07:28 -0500

    formatted the lib (#28)

--- extras/src/lib.rs
+++ extras/src/lib.rs
@@ -8,16 +8,16 @@ use wasmcloud_actor_http_server as http;
 
 #[actor::init]
 fn init() {
-  http::Handlers::register_handle_request(display_extras);
+    http::Handlers::register_handle_request(display_extras);
 }
 
 fn display_extras(_payload: http::Request) -> HandlerResult<http::Response> {
-  let extras = extras::default();
+    let extras = extras::default();
 
-  let result = json!(
-  { "random": extras.request_random(0, 100)?,
-    "guid" : extras.request_guid()?,
-    "sequence": extras.request_sequence()?,
-  });
-  Ok(http::Response::json(result, 200, "OK"))
+    let result = json!(
+    { "random": extras.request_random(0, 100)?,
+      "guid" : extras.request_guid()?,
+      "sequence": extras.request_sequence()?,
+    });
+    Ok(http::Response::json(result, 200, "OK"))
 }
