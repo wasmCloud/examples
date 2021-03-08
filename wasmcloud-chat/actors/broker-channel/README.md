ac892e61f881b150235516501b450b4bcecb538b
Kevin Hoffman <alothien@gmail.com>
2021-03-08 12:13:52 -0500

    Initial commit of broker channel actor implementation

---
+++ wasmcloud-chat/actors/broker-channel/README.md
@@ -0,0 +1,36 @@
+# Message Broker Channel
+
+```
+  FE                        .-----------------.                            .--------------.
+BROKER ---[Chat Request]--->|                  |-----[Channel Message]---->|               |
+                <---[ACK]---|                  |             <----[ACK]----|   Messages    |-------.
+                            |  Broker Channel  |                           |               |       |
+                            |                  |                            `--------------'       |
+  FE   <----[Message] ------|                  |                           .-----------------.     |
+BROKER                      |                  |<------[Chat Event]--------|  Backend Broker |<----'
+                            `------------------'                           `-----------------'
+```
+
+The message broker channel actor is one of the channel actors that provide a _proxy_ or _gateway_ to the back-end. In the case of
+the broker channel, the front-end is a `wasmcloud:messaging` capability provider and the back-end is the `messages` actor.
+
+This actor subscribes to message requests from the broker "front-end" and delivers them to the messages actor. It also subscribes to messages published on the back-end event subjects (`wcc.events.[user|room].*`), where the messages on the appropriate subjects will be forwarded to the appropriate front-end subject.
+
+## Subject Topology
+
+The following is a description of the relevant subjects used by this actor
+
+* `wcc.frontend.requests` - Subscribed to by the actor via the `frontend` named capability provider. Messages received here are then used to invoke the `messages` capability provider, with an appropriate acknowledgement returned.
+* `wcc.events.[user|room].*` - Subscribed to by the actor via the `backend` named capability provider. Messages received here are then forwarded to the front-end by publication to the frontend events subject.
+* `wcc.frontend.events.[user|room].*` - Subscribed to by all manner of front-end consumers. These subjects are where actual chat messages are published and then delivered to various user interfaces, e.g. via websockets or other proxies like Phoenix Liveview websites.
+
+## Provider Linking
+
+It is assumed that this actor must have _two_ capability provider links:
+
+* `backend` - This capability provider is configured with a connection to the back-end message broker (NATS)
+* `frontend` - This capability provider is configured with a connection to the front-end message broker (also NATS).
+
+The use of two different connections means that the back-end and front-end message broker systems do not share a security domain and that the only entity capable of communicating on both of these message brokers at the same time is this one actor.
+
+To illustrate the support for multiple different front-end consumers, the `MessageRequest` JSON message structure is _different_ than the message that is sent to the backend `messages` actor, making explicit use of the Anti-Corruption Layer pattern to isolate the different domains and prevent "bleeding" of logic and data across these boundaries.
---
+++ wasmcloud-chat/actors/broker-channel/src/lib.rs
@@ -0,0 +1,129 @@
+extern crate serde_json;
+
+use serde::{Deserialize, Serialize};
+use wapc_guest as guest;
+use wasmcloud_actor_core as actor;
+use wasmcloud_actor_extras as extras;
+use wasmcloud_actor_messaging as messaging;
+use wasmcloudchat_messages_interface as msgactor;
+
+use guest::prelude::*;
+
+const INBOUND_SUBJECT: &str = "wcc.frontend.requests";
+const BACKEND_SUBJECT_PREFIX: &str = "wcc.events.";
+const FRONTEND_SUBJECT_PREFIX: &str = "wcc.frontend.events.";
+const MSG_ACTOR_CALL_ALIAS: &str = "wasmcloud/chat/messages";
+const OP_PROCESS_MESSAGE: &str = "ProcessMessage";
+
+const CHANNEL_ID: &str = "messagebroker";
+const MSGTYPE_MESSAGE: &str = "MSG";
+
+const LINK_NAME_FRONTEND: &str = "frontend";
+const _LINK_NAME_BACKEND: &str = "backend";
+
+#[actor::init]
+fn init() {
+    messaging::Handlers::register_handle_message(handle_message);
+}
+
+fn handle_message(msg: messaging::BrokerMessage) -> HandlerResult<()> {
+    // TODO: handle request to obtain access token
+
+    if msg.subject == INBOUND_SUBJECT {
+        handle_inbound_message(msg)
+    } else if msg.subject.starts_with(BACKEND_SUBJECT_PREFIX) {
+        handle_outbound_message(msg)
+    } else {
+        Err(format!("Unrecognized subject {}", msg.subject).into())
+    }
+}
+
+/// Validates that the inbound request has a legitimate security authorization to
+/// publish messages.
+/// Takes an `InboundRequest` message delivered from the `frontend` link binding
+/// and converts that to a `ChannelMessage`, to be used in directly invoking
+/// the `messages` actor's `ProcessMessage` operation. Responds to the front-end
+/// with a copy of the acknowledgement received from the internal actor.
+fn handle_inbound_message(msg: messaging::BrokerMessage) -> HandlerResult<()> {
+    // TODO: validate access token
+    let req: InboundRequest = serde_json::from_slice(&msg.body)?;
+    let new_guid = extras::default()
+        .request_guid()?
+        .unwrap_or("???".to_string());
+    let channelmsg = msgactor::ChannelMessage {
+        created_on: req.created_on,
+        message_id: new_guid,
+        data: None,
+        origin_channel: CHANNEL_ID.to_string(),
+        origin_user_id: req.origin_user_id,
+        origin_room: req.origin_room,
+        target_url: req.target_url,
+        message_type: MSGTYPE_MESSAGE.to_string(),
+        message_text: req.message_text,
+    };
+    let ack: msgactor::ProcessAck =
+        actor::call_actor(MSG_ACTOR_CALL_ALIAS, OP_PROCESS_MESSAGE, &channelmsg)?;
+    let response = InboundResponse {
+        acknowledged: ack.processed,
+        error: ack.error,
+        message_id: ack.message_id,
+    };
+    if !msg.reply_to.is_empty() {
+        let _ = messaging::host(LINK_NAME_FRONTEND).publish(
+            msg.reply_to,
+            "".to_string(),
+            serde_json::to_vec(&response)?,
+        )?;
+    }
+    Ok(())
+}
+
+/// Takes a CloudEvent received from the back end and simply forwards it to the front-end
+/// by publishing it on the `frontend` link binding and replacing the subject prefix
+fn handle_outbound_message(msg: messaging::BrokerMessage) -> HandlerResult<()> {
+    let cloud_event: CloudEvent = serde_json::from_slice(&msg.body)?;
+    let subject = msg
+        .subject
+        .replace(BACKEND_SUBJECT_PREFIX, FRONTEND_SUBJECT_PREFIX);
+    let _ = messaging::host(LINK_NAME_FRONTEND).publish(
+        subject,
+        "".to_string(),
+        serde_json::to_vec(&cloud_event)?,
+    )?;
+    Ok(())
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Clone, Debug)]
+struct InboundRequest {
+    pub source_app_id: String,
+    pub source_app_version: String,
+    pub origin_room: Option<String>,
+    pub message_text: String,
+    pub origin_user_id: String,
+    pub target_url: String,
+    pub created_on: u64,
+    pub access_token: String,
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Clone, Debug)]
+struct InboundResponse {
+    pub message_id: String,
+    pub acknowledged: bool,
+    pub error: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Clone, Default)]
+pub struct CloudEvent {
+    #[serde(rename = "specversion")]
+    pub cloud_events_version: String,
+    #[serde(rename = "type")]
+    pub event_type: String,
+    pub source: String, // URI
+    #[serde(rename = "id")]
+    pub event_id: String,
+    #[serde(rename = "time")]
+    pub event_time: String,
+    #[serde(rename = "datacontenttype")]
+    pub content_type: String,
+    pub data: serde_json::Value,
+}
