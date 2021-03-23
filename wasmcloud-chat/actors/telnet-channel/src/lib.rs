5aa8c9caf48b136b1b3a39f903441e814bb25112
Brooks Townsend <brooksmtownsend@gmail.com>
2021-03-23 12:48:02 -0400

    initial working commit of telnet-channel, simply logging operations

--- wasmcloud-chat/actors/telnet-channel/src/lib.rs
+++ wasmcloud-chat/actors/telnet-channel/src/lib.rs
@@ -1,9 +1,32 @@
 extern crate wapc_guest as guest;
-use wasmcloud_actor_core as actor;
-
 use guest::prelude::*;
+use log::info;
+use messaging::BrokerMessage;
+use wasmcloud_actor_core as actor;
+use wasmcloud_actor_logging as logging;
+use wasmcloud_actor_messaging as messaging;
+use wasmcloud_actor_telnet as telnet;
 
 #[actor::init]
 fn init() {
-    // Register your message handlers here
+    messaging::Handlers::register_handle_message(handle_message);
+    telnet::Handlers::register_session_started(session_started);
+    telnet::Handlers::register_receive_text(receive_text);
+    logging::enable_macros();
+}
+
+fn handle_message(msg: BrokerMessage) -> HandlerResult<()> {
+    info!("RECEIVED MESSAGE: {:?}", msg);
+    Ok(())
+}
+
+fn session_started(msg: String) -> HandlerResult<bool> {
+    info!("telnet session started: {}", msg);
+    Ok(true)
+}
+
+//TODO: This is what happens when a person types in a message on the front end. Should then go ahead and send that message
+fn receive_text(session: String, text: String) -> HandlerResult<bool> {
+    info!("text ({}) received on session ({})", text, session);
+    Ok(true)
 }
