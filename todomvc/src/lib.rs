14c829eb8d358ba19cd48ba7d7f3a209ae59ac21
attriaayush <aayush.attri@red-badger.com>
2021-06-30 15:49:24 +0100

    creating a handler for get todos

--- todomvc/src/lib.rs
+++ todomvc/src/lib.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 use guest::prelude::*;
-use serde::{Deserialize, Serialize};
+use serde::{ser, Deserialize, Serialize};
 use wapc_guest as guest;
 use wasmcloud_actor_core as actor;
 use wasmcloud_actor_http_server as http;
@@ -44,13 +44,35 @@ fn create_todo(input: InputTodo) -> Result<Todo> {
         .set(id.to_string(), serde_json::to_string(&todo)?, 0)
         .map_err(|e| anyhow::anyhow!(e))?;
 
+    kv::default()
+        .push("all_ids".to_string(), id.to_string())
+        .map_err(|e| anyhow::anyhow!(e))?;
+
     Ok(todo)
+}
+
+fn get_all_todos() -> Result<Vec<Todo>> {
+    let ids = kv::default()
+        .range("all_ids".to_string(), 0, -1)
+        .map_err(|e| anyhow::anyhow!(e))?
+        .values;
+
+    let result: Result<Vec<_>, _> = ids.into_iter().map(|id| kv::default().get(id)).collect();
+    let res_vec = result.map_err(|e| anyhow::anyhow!(e))?;
+    let todos = res_vec
+        .into_iter()
+        .map(|response| serde_json::from_str(&response.value))
+        .collect::<Result<Vec<Todo>, _>>()?;
+
+    Ok(todos)
 }
 
 fn request_handler(msg: http::Request) -> HandlerResult<http::Response> {
     match (msg.path.as_ref(), msg.method.as_ref()) {
         ("/", "POST") => create_todo(serde_json::from_slice(&msg.body)?)
             .map(|todo| http::Response::json(todo, 200, "OK")),
+        ("/", "GET") => get_all_todos()
+            .map(|todos| http::Response::json(todos, 200, "OK")),
         (_, _) => Ok(http::Response::not_found()),
     }
     .or_else(|_| {
