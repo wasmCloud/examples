f0a28b01ad7c723abd35210f24d5a9385dbb04e2
Stuart Harris <stuart.harris@red-badger.com>
2021-06-30 13:34:59 +0100

    handle create todo

--- todomvc/src/lib.rs
+++ todomvc/src/lib.rs
@@ -1,10 +1,7 @@
-#[macro_use]
-extern crate serde_json;
-
-extern crate wapc_guest as guest;
+use anyhow::Result;
 use guest::prelude::*;
 use serde::{Deserialize, Serialize};
-use serde_json::Result;
+use wapc_guest as guest;
 use wasmcloud_actor_core as actor;
 use wasmcloud_actor_http_server as http;
 use wasmcloud_actor_keyvalue as kv;
@@ -14,33 +11,51 @@ pub fn init() {
     http::Handlers::register_handle_request(request_handler);
 }
 
-/**
- * Create a todo --> assigning a new key/value
- * Delete a todo --> delete by key
- */
-
+#[derive(Serialize, Deserialize)]
+struct InputTodo {
+    title: String,
+}
 #[derive(Serialize, Deserialize)]
 struct Todo {
-    id: u32,
+    id: i32,
     title: String,
     completed: bool,
 }
 
-fn create_todo(msg: std::vec::Vec<u8>) {
-    let key = kv::default().add("sequence.key".to_owned(), 1).unwrap();
-    let todo: Todo = serde_json::from_slice(&msg).unwrap();
-    let resp = kv::default().set(
-        format!("{}", key.value),
-        serde_json::to_string(&todo).unwrap(),
-        0,
-    );
+impl Todo {
+    fn new(id: i32, title: String) -> Self {
+        Self {
+            id,
+            title,
+            completed: false,
+        }
+    }
+}
+
+fn create_todo(input: InputTodo) -> Result<Todo> {
+    let id = kv::default()
+        .add("next_id".to_string(), 1)
+        .map_err(|e| anyhow::anyhow!(e))?
+        .value;
+
+    let todo = Todo::new(id, input.title);
+
+    kv::default()
+        .set(id.to_string(), serde_json::to_string(&todo)?, 0)
+        .map_err(|e| anyhow::anyhow!(e))?;
+
+    Ok(todo)
 }
 
 fn request_handler(msg: http::Request) -> HandlerResult<http::Response> {
     match (msg.path.as_ref(), msg.method.as_ref()) {
-        ("/", "POST") => create_todo(msg.body),
-        //    ("/", "DELETE") => delete_todo(&msg.body),
-        //    ("/", "GET") => get_todos(&msg.body),
+        ("/", "POST") => create_todo(serde_json::from_slice(&msg.body)?)
+            .map(|todo| http::Response::json(todo, 200, "OK")),
+        (_, _) => Ok(http::Response::not_found()),
     }
-    Ok(())
+    .or_else(|_| {
+        Ok(http::Response::internal_server_error(
+            "Something went wrong",
+        ))
+    })
 }
