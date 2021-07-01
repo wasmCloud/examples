72a66026d92c45ffc4a1b8906ccaa2ea2af65c44
attriaayush <aayush.attri@red-badger.com>
2021-07-01 16:32:26 +0100

    refactored the use of id to url everywhere

--- todomvc/src/lib.rs
+++ todomvc/src/lib.rs
@@ -26,9 +26,9 @@ struct Todo {
 }
 
 impl Todo {
-    fn new(id: i32, title: String) -> Self {
+    fn new(url: String, title: String) -> Self {
         Self {
-            url: format!("/api/{}", id),
+            url,
             title,
             completed: false,
         }
@@ -42,31 +42,31 @@ fn create_todo(input: InputTodo) -> Result<Todo> {
         .map_err(|e| anyhow::anyhow!(e))?
         .value;
 
-    let todo = Todo::new(id, input.title);
+    let todo = Todo::new(format!("/api/{}", id), input.title);
 
     kv::default()
-        .set(id.to_string(), serde_json::to_string(&todo)?, 0)
+        .set(todo.url.clone(), serde_json::to_string(&todo)?, 0)
         .map_err(|e| anyhow::anyhow!(e))?;
 
     kv::default()
-        .set_add("all_ids".to_string(), id.to_string())
+        .set_add("all_urls".to_string(), todo.url.clone())
         .map_err(|e| anyhow::anyhow!(e))?;
 
     Ok(todo)
 }
 
 fn get_all_todos() -> Result<Vec<Todo>> {
-    let ids = kv::default()
-        .set_query("all_ids".to_string())
+    let urls = kv::default()
+        .set_query("all_urls".to_string())
         .map_err(|e| anyhow::anyhow!(e))?
         .values;
 
-    ids.into_iter().map(|id| get_todo(id.parse()?)).collect()
+    urls.into_iter().map(|url| get_todo(&url)).collect()
 }
 
-fn get_todo(id: i32) -> Result<Todo> {
+fn get_todo(url: &str) -> Result<Todo> {
     let todo_str = kv::default()
-        .get(id.to_string())
+        .get(url.to_string())
         .map_err(|e| anyhow::anyhow!(e))?
         .value;
     let todo = serde_json::from_str(&todo_str)?;
@@ -75,59 +75,51 @@ fn get_todo(id: i32) -> Result<Todo> {
 }
 
 fn delete_all_todos() -> Result<()> {
-    let ids = kv::default()
-        .set_query("all_ids".to_string())
+    let urls = kv::default()
+        .set_query("all_urls".to_string())
         .map_err(|e| anyhow::anyhow!(e))?
         .values;
 
-    for id in ids {
-        delete_todo(id.parse()?)?
+    for url in urls {
+        delete_todo(&url)?
     }
 
     Ok(())
 }
 
-fn delete_todo(id: i32) -> Result<()> {
+fn delete_todo(url: &str) -> Result<()> {
     kv::default()
-        .set_remove("all_ids".to_string(), id.to_string())
+        .set_remove("all_urls".to_string(), url.to_string())
         .map_err(|e| anyhow::anyhow!(e))?;
 
     kv::default()
-        .del(id.to_string())
+        .del(url.to_string())
         .map_err(|e| anyhow::anyhow!(e))?;
 
     Ok(())
 }
 
-fn handle_delete_todo(path: &str) -> Result<http::Response> {
-    if let Ok(id) = path.trim_matches('/').parse() {
-        delete_todo(id).map(|()| http::Response::json("", 200, "OK"))
-    } else {
-        warn!("path is not a number {}", path);
-        Ok(http::Response::not_found())
-    }
+fn handle_delete_todo(url: &str) -> Result<http::Response> {
+    delete_todo(url).map(|()| http::Response::json("", 200, "OK"))
 }
 
-fn handle_get_todo(path: &str) -> Result<http::Response> {
-    if let Ok(id) = path.trim_matches('/').parse() {
-        get_todo(id).map(|todo| http::Response::json(todo, 200, "OK"))
-    } else {
-        warn!("path is not a number {}", path);
-        Ok(http::Response::not_found())
-    }
+fn handle_get_todo(url: &str) -> Result<http::Response> {
+    get_todo(url)
+        .map(|todo| http::Response::json(todo, 200, "OK"))
+        .or_else(|_| Ok(http::Response::not_found()))
 }
 
 fn request_handler(msg: http::Request) -> HandlerResult<http::Response> {
-    let trimmed_path = msg.path.trim_start_matches("/api");
-    trace!("incoming msg: {:?}, path: {:?}", msg, trimmed_path);
+    let trimmed_path = msg.path.trim_end_matches("/");
+    warn!("incoming msg: {:?}, path: {:?}", msg, trimmed_path);
 
     match (msg.method.as_ref(), trimmed_path) {
-        ("POST", "/") => create_todo(serde_json::from_slice(&msg.body)?)
+        ("POST", "/api") => create_todo(serde_json::from_slice(&msg.body)?)
             .map(|todo| http::Response::json(todo, 200, "OK")),
-        ("GET", "/") => get_all_todos().map(|todos| http::Response::json(todos, 200, "OK")),
-        ("GET", path) => handle_get_todo(path),
-        ("DELETE", "/") => delete_all_todos().map(|_| http::Response::ok()),
-        ("DELETE", path) => handle_delete_todo(path),
+        ("GET", "/api") => get_all_todos().map(|todos| http::Response::json(todos, 200, "OK")),
+        ("GET", url) => handle_get_todo(url),
+        ("DELETE", "/api") => delete_all_todos().map(|_| http::Response::ok()),
+        ("DELETE", url) => handle_delete_todo(url),
         (_, _) => {
             warn!("not even a thing: {:?}", msg);
             Ok(http::Response::not_found())
