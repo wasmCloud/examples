22a6762380835b4532ee1599276694d816fb14c7
Kaushik Shanadi <kaushik@cosmonic.com>
2022-01-11 15:37:28 -0500

    fix snyk issues

--- petclinic/petclinic-ui/craco.config.js
+++ petclinic/petclinic-ui/craco.config.js
@@ -1,11 +1,15 @@
 require('dotenv').config();
+// craco.config.js
 module.exports = {
-  style: {
-    postcss: {
-      plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-      ],
+  webpack: {
+    configure: (webpackConfig, { env, paths }) => {
+      webpackConfig.module.rules.push({
+        test: /\.m?js/,
+        resolve: {
+          fullySpecified: false
+        }
+      })
+      return webpackConfig;
     },
-  },
+  }
 }
