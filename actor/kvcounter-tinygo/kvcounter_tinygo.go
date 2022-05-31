6dbda99a9eff8c681f379891604ac18f166caa8b
Brooks Townsend <brooks@cosmonic.com>
2022-05-31 12:35:06 -0400

    used increment operation
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>

--- actor/kvcounter-tinygo/kvcounter_tinygo.go
+++ actor/kvcounter-tinygo/kvcounter_tinygo.go
@@ -3,7 +3,7 @@ package main
 import (
 	"strconv"
 
-	"github.com/wasmcloud/actor-tinygo"
+	actor "github.com/wasmcloud/actor-tinygo"
 	httpserver "github.com/wasmcloud/interfaces/httpserver/tinygo"
 	keyvalue "github.com/wasmcloud/interfaces/keyvalue/tinygo"
 )
@@ -17,26 +17,14 @@ type KvcounterTinygo struct{}
 func (e *KvcounterTinygo) HandleRequest(ctx *actor.Context, req httpserver.HttpRequest) (*httpserver.HttpResponse, error) {
 	kv := keyvalue.NewProviderKeyValue()
 
-	prev, err := kv.Get(ctx, "tinygo:count")
-	if err != nil {
-		return Failure("Couldn't query keyvalue store"), nil
-	}
-
-	count, err := strconv.Atoi(prev.Value)
-	if err != nil {
-		count = 0
-	}
-
-	newValue := strconv.Itoa(count + 1)
-	err = kv.Set(ctx, keyvalue.SetRequest{
-		Key:     "tinygo:count",
-		Value:   newValue,
-		Expires: 0,
+	newValue, err := kv.Increment(ctx, keyvalue.IncrementRequest{
+		Key: "tinygo:count", Value: 1,
 	})
+
 	if err != nil {
-		return Success("Couldn't set new value in keyvalue store"), err
+		return Success("Couldn't set increment value in keyvalue store"), err
 	}
-	return Success("Count: " + newValue), nil
+	return Success("Count: " + strconv.FormatInt(int64(newValue), 10)), nil
 }
 
 // Helper function to construct a successful HTTP Response
