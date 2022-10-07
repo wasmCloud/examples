71a1937f5c5ac6498fd0a738ae2bff2021ac67d1
Brooks Townsend <brooks@cosmonic.com>
2022-10-07 11:45:12 -0400

    Fixed petclinic ./run.sh dev script to not hang on docker compose (#168)
    * merge commit
    
    Signed-off-by: stevelr <steve@cosmonic.com>
    
    update provider references in run.sh
    
    Signed-off-by: stevelr <steve@cosmonic.com>
    
    remove pre-otp comment from README
    
    Signed-off-by: stevelr <steve@cosmonic.com>
    
    update to sqldb 0.7.1
    
    Signed-off-by: stevelr <steve@cosmonic.com>
    
    * fixed petclinic dev script
    
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>
    
    * updated petclinic data with more VIPs
    
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>
    
    set wasmCloud to depend on other services before starting (#167)
    
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>
    
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>
    
    update all examples to latest dependencies (#165)
    
    * update all examples to latest dependencies, remove unused dependencies; deprecate pre-otp
    
    Signed-off-by: stevelr <steve@cosmonic.com>
    
    * update provider references in run.sh
    
    Signed-off-by: stevelr <steve@cosmonic.com>
    
    * remove pre-otp comment from README
    
    Signed-off-by: stevelr <steve@cosmonic.com>
    
    * update to sqldb 0.7.1
    
    Signed-off-by: stevelr <steve@cosmonic.com>
    
    Signed-off-by: stevelr <steve@cosmonic.com>
    
    * added Douglas
    
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>
    
    Signed-off-by: stevelr <steve@cosmonic.com>
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>
    Co-authored-by: stevelr <steve@cosmonic.com>
    Signed-off-by: ks2211 <shanadik6@gmail.com>

--- petclinic/create_data.sql
+++ petclinic/create_data.sql
@@ -4,14 +4,24 @@ insert into vets values (3, 'Pear', 'It', 'Birds');
 insert into pettypes values (1, 'Dog');
 insert into pettypes values (2, 'Cat');
 insert into pettypes values (3, 'Bird');
+-- Owners
 insert into owners (id, address, city, email, firstname, lastname, telephone) values (4, '1234 Street Lane', 'Metropolis', 'brooks@email.com', 'Brooks', 'Townsend', '123-123-1234');
 insert into owners (id, address, city, email, firstname, lastname, telephone) values (1, '1234 Street Lane', 'Metropolis', 'kevin@email.com', 'Kevin', 'Hoffman', '123-123-1234');
 insert into owners (id, address, city, email, firstname, lastname, telephone) values (3, '1234 Street Lane', 'Metropolis', 'taylor@email.com', 'Taylor', 'Thomas', '123-123-1234');
 insert into owners (id, address, city, email, firstname, lastname, telephone) values (2, '1234 Street Lane', 'Metropolis', 'connor@email.com', 'Connor', 'Smith', '123-123-1234');
+insert into owners (id, address, city, email, firstname, lastname, telephone) values (5, '1234 Street Lane', 'Metropolis', 'caroline@email.com', 'Caroline', 'Tarbett', '123-123-1234');
+insert into owners (id, address, city, email, firstname, lastname, telephone) values (6, '1234 Street Lane', 'Metropolis', 'bailey@email.com', 'Bailey', 'Hayes', '123-123-1234');
+insert into owners (id, address, city, email, firstname, lastname, telephone) values (7, '1234 Street Lane', 'Metropolis', 'lachlan@email.com', 'Lachlan', 'Heywood', '123-123-1234');
+-- Pets
 insert into pets (id, pettype, name, bday, bmonth, byear, ownerid) values (1, 1, 'Archie', 5, 3, 2019, 4);
 insert into pets (id, pettype, name, bday, bmonth, byear, ownerid) values (2, 1, 'Sage', 4, 3, 2020, 1);
 insert into pets (id, pettype, name, bday, bmonth, byear, ownerid) values (3, 1, 'Chex', 7, 12, 2016, 3);
 insert into pets (id, pettype, name, bday, bmonth, byear, ownerid) values (4, 1, 'Desmond', 11, 12, 2018, 2);
 insert into pets (id, pettype, name, bday, bmonth, byear, ownerid) values (5, 1, 'Abby', 11, 10, 2011, 1);
+insert into pets (id, pettype, name, bday, bmonth, byear, ownerid) values (6, 2, 'Samson', 12, 12, 2012, 5);
+insert into pets (id, pettype, name, bday, bmonth, byear, ownerid) values (7, 1, 'Harley Quinn', 12, 12, 2012, 6);
+insert into pets (id, pettype, name, bday, bmonth, byear, ownerid) values (8, 1, 'Loki Odin', 12, 12, 2012, 6);
+insert into pets (id, pettype, name, bday, bmonth, byear, ownerid) values (9, 1, 'Nelson', 12, 12, 2012, 7);
+insert into pets (id, pettype, name, bday, bmonth, byear, ownerid) values (10, 1, 'Douglas', 5, 3, 2019, 4);
 insert into visits (day, month, year, description, petid, vetid, ownerid, hour, minute) values (2, 2, 2022, 'Annual Checkup, diagnosis: very good boy', 1, 1, 4, 12, 12);
 insert into visits (day, month, year, description, petid, vetid, ownerid, hour, minute) values (9, 6, 2022, 'Her growl sounds like Donald Duck', 5, 1, 1, 16, 10);
