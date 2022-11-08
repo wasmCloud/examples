9121249e6dc51bd634c74800d42277a2edb9a5ad
Kaushik Shanadi <kaushik@cosmonic.com>
2022-11-07 21:19:27 -0500

    add responsive position for animals
    Signed-off-by: ks2211 <shanadik6@gmail.com>

--- petclinic/actors/ui/src/App.js
+++ petclinic/actors/ui/src/App.js
@@ -27,8 +27,8 @@ function App() {
       <Nav changePage={(page) => setPage(page)} />
       <div className="container mx-auto pt-8 relative">
         <ErrorBoundary>
-          <img src="/images/cat.png" className="absolute top-0 -left-[40%] opacity-75 -z-10" />
-          <img src="/images/dog.png" className="absolute top-1/2 -right-[38%] opacity-75 -z-10" />
+          <img src="/images/cat.png" className="absolute top-0 sm:-left-[37%] -left-[40%] opacity-75 -z-10" />
+          <img src="/images/dog.png" className="absolute sm:-right-[35%] top-1/2 -right-[38%] opacity-75 -z-10" />
           {renderPage()}
         </ErrorBoundary>
       </div>
