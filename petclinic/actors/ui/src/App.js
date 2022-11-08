68b181ac25ceab9ae43b067112b0e6dd3bf29381
Kaushik Shanadi <kaushik@cosmonic.com>
2022-11-07 21:19:27 -0500

    add responsive position for animals
    Signed-off-by: ks2211 <shanadik6@gmail.com>

--- petclinic/actors/ui/src/App.js
+++ petclinic/actors/ui/src/App.js
@@ -27,18 +27,8 @@ function App() {
       <Nav changePage={(page) => setPage(page)} />
       <div className="container mx-auto px-8 pt-8 relative">
         <ErrorBoundary>
-<<<<<<< HEAD
-<<<<<<< HEAD
           <img src="/images/cat.png" className="absolute top-0 sm:-left-[30%] -left-[38%] opacity-75 -z-10" />
           <img src="/images/dog.png" className="absolute top-1/2 sm:-right-[30%] -right-[35%] opacity-75 -z-10" />
-=======
-          <img src="/images/cat.png" className="absolute top-0 sm:-left-[37%] -left-[40%] opacity-75 -z-10" />
-          <img src="/images/dog.png" className="absolute sm:-right-[35%] top-1/2 -right-[38%] opacity-75 -z-10" />
->>>>>>> 7c56d36 (add responsive position for animals)
-=======
-          <img src="/images/cat.png" className="absolute top-0 sm:-left-[30%] -left-[38%] opacity-75 -z-10" />
-          <img src="/images/dog.png" className="absolute top-1/2 sm:-right-[30%] -right-[35%] opacity-75 -z-10" />
->>>>>>> b21b0ba (fix responsive issues, fix positions)
           {renderPage()}
         </ErrorBoundary>
       </div>
