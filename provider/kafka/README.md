a155f236757e47bdf9bc46dfca446ac8ae8ecb38
Brooks Townsend <brooks@cosmonic.com>
2022-11-23 12:00:55 -0500

    implemented pub functionality
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>

--- provider/kafka/README.md
+++ provider/kafka/README.md
@@ -1,20 +1,14 @@
 # Kafka Capability Provider
-This capability provider is an implementation of the `wasmcloud:messaging` contract. It exposes publish, request, and subscribe functionality to actors.
+
+This capability provider is an implementation of the `wasmcloud:messaging` contract. It exposes publish, request, and subscribe functionality to actors to operate on Kafka topics when connecting to a Kafka-compatible API. At the time of writing, this provider was tested and works well with Apache Kafka and Redpanda.
 
 ## Link Definition Configuration Settings
 
+| Property | Description                                                                                                                                                                                                                                                                |
+| :------- | :------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- |
+| `HOSTS`  | A comma-separated list of bootstrap server hosts. For example, `HOSTS=127.0.0.1:9092,127.0.0.1:9093`. A single value is accepted as well, and the default value is the Kafka default of `127.0.0.1:9092`. This will be used for both the consumer and producer connections |
+| `TOPIC`  | The Kafka topic you wish to consume. Any messages on this topic will be forwarded to this actor for processing                                                                                                                                                             |
 
-## Installing Kafka
-To install
-```
-brew install kafka
-```
+## Limitations
 
-To start the Kafka service on a mac, use
-```
-brew services start zookeeper
-brew services start kafka
-```
-To create a topic, use `make create-topic`, it's just for testing tho
-
-
+This capability provider only implements the very basic Kafka functionality of producing to a topic and consuming a topic. Because of this, advanced Kafka users may find that this is implemented without specific optimizations or options and we welcome any additions to this client. Additionally, running multiple copies of this provider across different hosts was not tested during development, and it's possible that multiple instances of this provider will cause unexpected behavior like duplicate message delivery.
--- provider/kafka/src/main.rs
+++ provider/kafka/src/main.rs
@@ -1,113 +1,161 @@
 //! Implementation for wasmcloud:messaging
 //!
 use std::{
+    collections::{BTreeMap, HashMap},
     convert::Infallible,
     sync::{Arc, RwLock},
 };
 
 use futures::StreamExt;
-use rskafka::client::{
-    consumer::{StartOffset, StreamConsumerBuilder},
-    ClientBuilder,
+use rskafka::{
+    client::{
+        consumer::{StartOffset, StreamConsumerBuilder},
+        partition::Compression,
+        ClientBuilder,
+    },
+    record::{Record, RecordAndOffset},
+    time::OffsetDateTime,
 };
-
-use kafka::consumer::Consumer;
-use tokio::runtime::Handle;
-use tracing::{debug, instrument};
+use tokio::task::JoinHandle;
+use tracing::{debug, instrument, warn};
 use wasmbus_rpc::{core::LinkDefinition, provider::prelude::*};
 use wasmcloud_interface_messaging::{
     MessageSubscriber, MessageSubscriberSender, Messaging, MessagingReceiver, PubMessage,
     ReplyMessage, RequestMessage, SubMessage,
 };
 
-const KAFKA_HOSTS: &str = "WASMCLOUD_KAFKA_HOSTS";
+/// Linkdef value for hosts, accepted as a comma separated string
+const KAFKA_HOSTS: &str = "HOSTS";
 const DEFAULT_HOST: &str = "127.0.0.1:9092";
+/// Linkdef value for topic, accepted as a single string
+const KAFKA_TOPIC: &str = "TOPIC";
+const DEFAULT_TOPIC: &str = "my-topic";
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // handle lattice control messages and forward rpc to the provider dispatch
-    // returns when provider receives a shutdown control message
-    let hosts = std::env::var(KAFKA_HOSTS)
-        .unwrap_or_else(|_| DEFAULT_HOST.to_string())
-        .trim()
-        .split(',')
-        .map(|s| s.to_string())
-        .collect::<Vec<String>>();
-    // get partition client
-    let client = ClientBuilder::new(hosts).build().await.unwrap();
-    let partition_client = Arc::new(client.partition_client("my-topic", 0).unwrap());
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    provider_main(
+        KafkaProvider::default(),
+        Some("Generic Kafka-API Provider".to_string()),
+    )?;
 
-    // construct stream consumer
-    let mut stream = StreamConsumerBuilder::new(partition_client, StartOffset::Earliest)
-        .with_max_wait_ms(100)
-        .build();
-
-    // consume data
-    while let Some(Ok((record, water_mark))) = stream.next().await {
-        println!(
-            "FROM REDPANDA {:?} {:?}",
-            String::from_utf8_lossy(&record.record.value.unwrap()),
-            water_mark
-        );
-    }
-
-    // let kafka = KafkaProvider::new(hosts);
-    //TODO: adjust args
-    // provider_run(KafkaProvider::new(), Some("Kafka Provider".to_string()))?;
-    // println!("consumer: {:?}", kafka.consumer);
-    // println!(
-    //     "client stuff: {:?}",
-    //     kafka.consumer.write().unwrap();
-    // );
-    // consume_messages(kafka.consumer).unwrap();
-
-    std::thread::park();
     eprintln!("Kafka provider exiting");
     Ok(())
 }
 
-/// Implementation for wasmcloud:messaging
+#[derive(Clone)]
+/// A struct that contains a consumer task handler and the host connection strings
+struct KafkaConnection {
+    connection_hosts: Vec<String>,
+    consumer_handle: Arc<JoinHandle<()>>,
+}
+
 #[derive(Clone, Provider)]
 #[services(Messaging)]
 struct KafkaProvider {
-    // producer: Arc<RwLock<Producer>>,
-    consumer: Arc<RwLock<Consumer>>,
+    // Map of actor ID to the JoinHandle where messages are consumed. When a link is put
+    // we spawn a tokio::task to handle messages, and on delete the task is closed
+    connections: Arc<RwLock<HashMap<String, KafkaConnection>>>,
 }
 
-impl KafkaProvider {
-    fn new(hosts: Vec<String>) -> Self {
-        Self {
-            //TODO: configure this
-            // producer: Arc::new(RwLock::new(
-            //     Producer::from_hosts(hosts.clone()).create().unwrap(),
-            // )),
-            consumer: Arc::new(RwLock::new(
-                Consumer::from_hosts(hosts)
-                    .with_topic("my-topic".to_owned())
-                    // .with_fallback_offset(FetchOffset::Earliest)
-                    .with_group("my-group-unique".to_owned())
-                    // .with_offset_storage(GroupOffsetStorage::Kafka)
-                    .create()
-                    .unwrap(),
-            )),
+impl Default for KafkaProvider {
+    fn default() -> Self {
+        KafkaProvider {
+            connections: Arc::new(RwLock::new(HashMap::new())),
         }
     }
 }
 // use default implementations of provider message handlers
 impl ProviderDispatch for KafkaProvider {}
 
-/// Handle provider control commands
-/// put_link (new actor link command), del_link (remove link command), and shutdown
 #[async_trait]
 impl ProviderHandler for KafkaProvider {
-    /// Provider should perform any operations needed for a new link,
-    /// including setting up per-actor resources, and checking authorization.
-    /// If the link is allowed, return true, otherwise return false to deny the link.
     #[instrument(level = "info", skip(self))]
     async fn put_link(&self, ld: &LinkDefinition) -> RpcResult<bool> {
         debug!("putting link for actor {:?}", ld);
-        // let consumer = self.consumer.clone();
-        // let ld = ld.clone();
+        // Collect comma separated hosts into a Vec<String>
+        let hosts = ld
+            .values
+            .get(KAFKA_HOSTS)
+            .cloned()
+            .unwrap_or_else(|| DEFAULT_HOST.to_string())
+            .trim()
+            .split(',')
+            .map(|s| s.to_string())
+            .collect::<Vec<String>>();
+
+        // Retrieve or use default topic, trimming off extra whitespace
+        let topic = ld
+            .values
+            .get(KAFKA_TOPIC)
+            .cloned()
+            .unwrap_or_else(|| DEFAULT_TOPIC.to_string())
+            .trim()
+            .to_string();
+
+        // Do some basic validation before spawning off in a thread
+        if let Ok(client) = ClientBuilder::new(hosts.clone()).build().await {
+            if let Ok(partition_client) = client.partition_client(&topic, 0) {
+                // Clone for moving into thread
+                let ld = ld.clone();
+                let actor_id = ld.actor_id.clone();
+                let join = tokio::task::spawn(async move {
+                    // construct stream consumer
+                    let mut stream =
+                    // StartOffset::Latest only processes new messages, but Earliest will send every message.
+                    // This could be a linkdef tunable value in the future
+                        StreamConsumerBuilder::new(Arc::new(partition_client), StartOffset::Latest)
+                            .with_max_wait_ms(100)
+                            .build();
+
+                    // Continue to pull records off the stream until it closes
+                    while let Some(Ok((
+                        RecordAndOffset {
+                            record:
+                                Record {
+                                    value: Some(message),
+                                    ..
+                                },
+                            ..
+                        },
+                        _water_mark,
+                    ))) = stream.next().await
+                    {
+                        let actor = MessageSubscriberSender::for_actor(&ld);
+                        if let Err(e) = actor
+                            .handle_message(
+                                &Context::default(),
+                                &SubMessage {
+                                    body: message,
+                                    reply_to: None,
+                                    subject: topic.to_owned(),
+                                },
+                            )
+                            .await
+                        {
+                            eprintln!("Unable to send subscription: {:?}", e);
+                        }
+                    }
+                });
+
+                let mut connections = self.connections.write().unwrap();
+                connections.insert(
+                    actor_id,
+                    KafkaConnection {
+                        consumer_handle: Arc::new(join),
+                        connection_hosts: hosts,
+                    },
+                );
+            } else {
+                warn!(
+                    "Could not create partition client for actor {}, messages won't be received",
+                    ld.actor_id
+                );
+            }
+        } else {
+            warn!(
+                "Could not create Kafka client for actor {}, messages won't be received",
+                ld.actor_id
+            );
+        }
 
         Ok(true)
     }
@@ -116,6 +164,17 @@ impl ProviderHandler for KafkaProvider {
     #[instrument(level = "info", skip(self))]
     async fn delete_link(&self, actor_id: &str) {
         debug!("deleting link for actor {}", actor_id);
+
+        let mut connections = self.connections.write().unwrap();
+        if let Some(KafkaConnection {
+            consumer_handle: handle,
+            ..
+        }) = connections.remove(actor_id)
+        {
+            handle.abort()
+        } else {
+            debug!("Linkdef deleted for non-existent consumer, ignoring")
+        }
     }
 
     /// Handle shutdown request with any cleanup necessary
@@ -128,9 +187,56 @@ impl ProviderHandler for KafkaProvider {
 #[async_trait]
 impl Messaging for KafkaProvider {
     #[instrument(level = "debug", skip(self, msg), fields(subject = %msg.subject, reply_to = ?msg.reply_to, body_len = %msg.body.len()))]
-    async fn publish(&self, _ctx: &Context, msg: &PubMessage) -> RpcResult<()> {
+    async fn publish(&self, ctx: &Context, msg: &PubMessage) -> RpcResult<()> {
         debug!("Publishing message: {:?}", msg);
-        Err(RpcError::NotImplemented)
+        //TODO: kill these unwraps
+        let config = self
+            .connections
+            .read()
+            .unwrap()
+            .get(&ctx.actor.clone().unwrap())
+            .cloned()
+            .unwrap();
+
+        let client = ClientBuilder::new(config.connection_hosts)
+            .build()
+            .await
+            .unwrap();
+
+        // Ensure topic exists
+        let controller_client = client.controller_client().unwrap();
+        if let Err(e) = controller_client
+            .create_topic(
+                msg.subject.to_owned(),
+                1,     // partition
+                1,     // replication factor
+                1_000, // timeout (ms)
+            )
+            .await
+        {
+            warn!("could not create topic: {:?}", e)
+        }
+
+        // get a partition-bound client
+        let partition_client = client
+            .partition_client(
+                msg.subject.to_owned(),
+                0, // partition
+            )
+            .unwrap();
+
+        // produce some data
+        let record = Record {
+            key: None,
+            value: Some(msg.body.clone()),
+            headers: BTreeMap::from([("source".to_owned(), b"wasm".to_vec())]),
+            timestamp: OffsetDateTime::now_utc(),
+        };
+        partition_client
+            .produce(vec![record], Compression::default())
+            .await
+            .unwrap();
+        Ok(())
     }
 
     #[instrument(level = "debug", skip(self, msg), fields(subject = %msg.subject))]
@@ -138,35 +244,4 @@ impl Messaging for KafkaProvider {
         debug!("Sending message request: {:?}", msg);
         Err(RpcError::NotImplemented)
     }
-}
-
-fn consume_messages(consumer: Arc<RwLock<Consumer>>) -> RpcResult<()> {
-    tokio::task::spawn_blocking(move || loop {
-        loop {
-            let mut consumer = consumer.write().unwrap();
-            if let Ok(msgs) = consumer.poll() {
-                println!("msg sets");
-                for ms in msgs.iter() {
-                    for m in ms.messages() {
-                        println!("{}", String::from_utf8_lossy(m.value));
-                    }
-                    consumer.consume_messageset(ms).unwrap();
-                }
-                consumer.commit_consumed().unwrap();
-            }
-        }
-        // let actor = MessageSubscriberSender::for_actor(&ld);
-        // let handle = Handle::current();
-        // if let Err(e) = handle.block_on(actor.handle_message(
-        //     &Context::default(),
-        //     &SubMessage {
-        //         body: m.value.to_vec(),
-        //         reply_to: None,
-        //         subject: "my-topic".to_owned(),
-        //     },
-        // )) {
-        //     eprintln!("Unable to send subscription: {:?}", e);
-        // }
-    });
-    Ok(())
 }
