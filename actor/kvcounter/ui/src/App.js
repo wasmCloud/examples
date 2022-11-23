5bf1b41e89530d0ef3cc7ca117c10a258ab0d87e
ks2211 <shanadik6@gmail.com>
2022-11-23 11:06:57 -0500

    pr fixes, remove overflow
    Signed-off-by: ks2211 <shanadik6@gmail.com>

--- actor/kvcounter/ui/src/App.js
+++ actor/kvcounter/ui/src/App.js
@@ -1,7 +1,6 @@
 import Confetti from 'react-confetti'
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import api from './Api';
-import Fireworks from '@fireworks-js/react';
 
 function getWindowDimensions() {
   const { innerWidth: width, innerHeight: height } = window;
@@ -17,9 +16,6 @@ function App() {
   const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
   const [counter, setCounter] = useState(0);
   const [showConfetti, setShowConfetti] = useState(false);
-  const [showFireworks, setShowFireworks] = useState(false);
-
-  const fireworksRef = useRef(null);
 
 
   const getCounter = async (key) => {
@@ -37,42 +33,23 @@ function App() {
 
   useEffect(() => {
     if (counter > 0) {
-      if (counter % 2 === 0) {
-        setShowConfetti(true)
-        setShowFireworks(false)
-      } else {
-        setShowFireworks(true);
-        setShowConfetti(false)
-      }
+      setShowConfetti(true)
     }
   }, [counter])
 
-  useEffect(() => {
-    if (!showFireworks) {
-      if (fireworksRef.current && fireworksRef.current.isRunning) {
-        fireworksRef.current.stop();
-      }
-    }
-  }, [showFireworks])
-
-  console.log(windowDimensions.height)
   return (
-    <div className="flex items-center justify-center h-screen relative">
+    <div className="flex items-center justify-center h-screen relative overflow-hidden">
       <div className="z-40">
-        <h1 className="absolute bottom-0 right-2 text-wasmcloudGray text-[72px]">
-          <span className="text-wasmcloudGreen-light">+</span>
-          <span>KVCounter</span>
-        </h1>
-        <img src="/wasmcloud_logo.svg" className="absolute -bottom-[12%] right-2 w-1/3 h-1/2" alt='logo' />
+        <img src="/wasmcloud_logo.svg" className="absolute -bottom-[15%] right-2 w-1/3 h-1/2 max-h-1/2" height="50%" alt='logo' />
         <div className="flex flex-col space-y-2 mx-auto">
-          <div className="flex space-x-2">
+          <div className="flex flex-col space-y-2">
             <input
               id="bucket" name="bucket"
-              placeholder='Enter Bucket Name' onChange={(e) => setBucket(e.target.value)}
-              className="p-4 mx-auto h-[100px] text-center w-4/5 text-[88px] text-wasmcloudGray rounded-md border border-wasmcloudGreen-light" />
+              placeholder='Bucket...' onChange={(e) => setBucket(e.target.value)}
+              className="p-4 mx-auto w-[350px] h-[100px] text-center w-4/5 text-[88px] text-wasmcloudGray rounded-md border border-wasmcloudGreen-light" />
             <button
               onClick={() => getCounter(bucket)}
-              className="bg-wasmcloudGreen-light rounded-md h-[40px] w-[125px] hover:bg-wasmcloudGreen-dark text-white font-bold py-2 px-4 my-auto mx-auto">
+              className="bg-wasmcloudGreen-light rounded-md h-[40px] w-[350px] hover:bg-wasmcloudGreen-dark text-white font-bold py-2 px-4 my-auto mx-auto">
               Increment
             </button>
           </div>
@@ -85,19 +62,6 @@ function App() {
         // onConfettiComplete={() => setShowConfetti(false)}
         recycle={true}
         gravity={0.2}
-      />}
-      {showFireworks && <Fireworks
-        ref={fireworksRef}
-        explosion={20}
-        options={{ opacity: 0.5 }}
-        style={{
-          top: 0,
-          left: 0,
-          position: 'absolute',
-          width: '100%',
-          height: '100%',
-          background: '#fff'
-        }}
       />}
     </div>
 
