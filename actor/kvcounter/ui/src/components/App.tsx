4b3dbdef0a4b5f36a9efb65d8345fdab56a51d99
Lachlan Heywood <lachieh@users.noreply.github.com>
2022-11-29 18:25:23 -0500

    update with more todos
    Signed-off-by: Lachlan Heywood <lachieh@users.noreply.github.com>

--- actor/kvcounter/ui/src/components/App.tsx
+++ actor/kvcounter/ui/src/components/App.tsx
@@ -1,5 +1,5 @@
 import { Fireworks, FireworksHandlers } from "@fireworks-js/react"
-import { useState, useEffect, useRef } from "react"
+import { useState, useEffect, useRef, FormEvent } from "react"
 import api from "../services/ApiService"
 import { ReactComponent as Logo } from "../assets/logo.svg"
 
@@ -7,7 +7,7 @@ function App() {
   const [bucket, setBucket] = useState("")
   const [count, setCount] = useState(0)
   const fireworks = useRef<FireworksHandlers>(null)
-
+  
   const updateCount = async (key?: string) => {
     try {
       const response = await api.increment(key)
@@ -18,7 +18,14 @@ function App() {
     }
   }
 
-  const handleClick = () => {
+  useEffect(() => {
+    // TODO: Update fireworks canvas size when screen is resized
+    
+  }, [])
+  
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault()
+    // TODO: work out how to reset count when bucket name changes
     updateCount(bucket).then(newCount => fireworks.current?.launch(newCount - count))
   }
 
@@ -29,7 +36,7 @@ function App() {
   return (
     <div className="h-full flex flex-col items-center justify-center">
       <div className="flex flex-col gap-2">
-        <div className="flex flex-wrap gap-2">
+        <form className="flex flex-wrap gap-2" onSubmit={handleSubmit}>
           <input
             id="bucket"
             name="bucket"
@@ -38,12 +45,12 @@ function App() {
             className="mx-auto px-2 py-1.5 text-center w-56 max-w-full text-wasmcloudGray rounded-md border border-wasmcloudGreen-light"
           />
           <button
-            onClick={handleClick}
+            type="submit"
             className="bg-wasmcloudGreen-light w-56 max-w-full rounded-md hover:bg-wasmcloudGreen-dark text-white font-bold py-2 px-4 my-auto mx-auto"
           >
             Increment
           </button>
-        </div>
+        </form>
         <h2 className="text-7xl mt-5 mx-auto font-bolder text-wasmcloudGreen-light">
           {count}
         </h2>
