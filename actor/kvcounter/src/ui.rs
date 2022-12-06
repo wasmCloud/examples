6fd9d6f83da72efbf79913ff5cbcea0dd302ff74
Brooks Townsend <brooks@cosmonic.com>
2022-12-06 13:11:14 -0500

    refactored kvcounter get_asset to ui
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>

--- actor/kvcounter/src/ui.rs
+++ actor/kvcounter/src/ui.rs
@@ -1,6 +1,25 @@
 use rust_embed::RustEmbed;
+use wasmbus_rpc::actor::prelude::RpcResult;
+use wasmcloud_interface_httpserver::HttpResponse;
 
 /// This embeds the compiled static assets inside of the WebAssembly module
 #[derive(RustEmbed)]
 #[folder = "./ui/build"]
 pub(crate) struct Asset;
+
+pub(crate) fn get_asset(asset: String) -> RpcResult<HttpResponse> {
+    let asset_request = if asset.trim() == "/" || asset.trim().is_empty() {
+        "index.html"
+    } else {
+        asset.trim().trim_start_matches('/')
+    };
+
+    if let Some(static_asset) = Asset::get(asset_request) {
+        Ok(HttpResponse {
+            body: Vec::from(static_asset.data),
+            ..Default::default()
+        })
+    } else {
+        Ok(HttpResponse::not_found())
+    }
+}
