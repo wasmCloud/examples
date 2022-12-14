a18e976e0dd1e297ee6c1f55e3066240215cfe91
Brooks Townsend <brooks@cosmonic.com>
2022-12-14 09:51:06 -0500

    added Patrick to petclinic (#185)
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>
    
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>

--- petclinic/create_data.sql
+++ petclinic/create_data.sql
@@ -12,6 +12,7 @@ insert into owners (id, address, city, email, firstname, lastname, telephone) va
 insert into owners (id, address, city, email, firstname, lastname, telephone) values (5, '1234 Street Lane', 'Metropolis', 'caroline@email.com', 'Caroline', 'Tarbett', '123-123-1234');
 insert into owners (id, address, city, email, firstname, lastname, telephone) values (6, '1234 Street Lane', 'Metropolis', 'bailey@email.com', 'Bailey', 'Hayes', '123-123-1234');
 insert into owners (id, address, city, email, firstname, lastname, telephone) values (7, '1234 Street Lane', 'Metropolis', 'lachlan@email.com', 'Lachlan', 'Heywood', '123-123-1234');
+insert into owners (id, address, city, email, firstname, lastname, telephone) values (8, '1234 Street Lane', 'Metropolis', 'patrick@email.com', 'Patrick', 'Gray', '123-123-1234');
 -- Pets
 insert into pets (id, pettype, name, bday, bmonth, byear, ownerid) values (1, 1, 'Archie', 5, 3, 2019, 4);
 insert into pets (id, pettype, name, bday, bmonth, byear, ownerid) values (2, 1, 'Sage', 4, 3, 2020, 1);
@@ -23,5 +24,6 @@ insert into pets (id, pettype, name, bday, bmonth, byear, ownerid) values (7, 1,
 insert into pets (id, pettype, name, bday, bmonth, byear, ownerid) values (8, 1, 'Loki Odin', 12, 12, 2012, 6);
 insert into pets (id, pettype, name, bday, bmonth, byear, ownerid) values (9, 1, 'Nelson', 12, 12, 2012, 7);
 insert into pets (id, pettype, name, bday, bmonth, byear, ownerid) values (10, 1, 'Douglas', 5, 3, 2019, 4);
+insert into pets (id, pettype, name, bday, bmonth, byear, ownerid) values (11, 1, 'Cinza', 4, 18, 2019, 8);
 insert into visits (day, month, year, description, petid, vetid, ownerid, hour, minute) values (2, 2, 2022, 'Annual Checkup, diagnosis: very good boy', 1, 1, 4, 12, 12);
 insert into visits (day, month, year, description, petid, vetid, ownerid, hour, minute) values (9, 6, 2022, 'Her growl sounds like Donald Duck', 5, 1, 1, 16, 10);
