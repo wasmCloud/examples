4e5d3cb9cdcd7f36e937e500ace2c99e0bb95a3c
Brooks Townsend <brooks@cosmonic.com>
2023-04-19 08:03:04 +0200

    added wasmcloud.toml for each actor
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>

--- actor/README.md
+++ actor/README.md
@@ -1,2 +1,3 @@
 ## Actor Examples
-This directory contains example actors that are useful either on their own for illustrating various aspects of the wasmCloud runtime or as part of samples used by our [documentation](https://wasmcloud.dev).
+
+This directory contains example actors that are useful either on their own for illustrating various aspects of the wasmCloud runtime or as part of samples used by our [documentation](https://wasmcloud.com).
--- actor/hello/src/lib.rs
+++ actor/hello/src/lib.rs
@@ -5,25 +5,10 @@ use wasmcloud_interface_httpserver::{HttpRequest, HttpResponse, HttpServer, Http
 #[services(Actor, HttpServer)]
 struct HelloActor {}
 
-/// Implementation of HttpServer methods
+/// Implementation of the HTTP server capability
 #[async_trait]
 impl HttpServer for HelloActor {
-    /// Returns a greeting, "Hello World", in the response body.
-    /// If the request contains a query parameter 'name=NAME', the
-    /// response is changed to "Hello NAME"
-    async fn handle_request(
-        &self,
-        _ctx: &Context,
-        req: &HttpRequest,
-    ) -> std::result::Result<HttpResponse, RpcError> {
-        let text = form_urlencoded::parse(req.query_string.as_bytes())
-            .find(|(n, _)| n == "name")
-            .map(|(_, v)| v.to_string())
-            .unwrap_or_else(|| "World".to_string());
-
-        Ok(HttpResponse {
-            body: format!("Hello {}", text).as_bytes().to_vec(),
-            ..Default::default()
-        })
+    async fn handle_request(&self, _ctx: &Context, _req: &HttpRequest) -> RpcResult<HttpResponse> {
+        Ok(HttpResponse::ok("Hello, World!"))
     }
 }
